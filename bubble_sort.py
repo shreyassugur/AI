cb313a7853c39d8d872610da58bedb75f3610d5c
shreyassugur <shreyasramesh.cs24@bmsce.ac.in>
2026-09-23 13:20:39 +0530

    Add files via upload

---
+++ bubble_sort.py
@@ -0,0 +1,11 @@
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+n = len(arr)
+
+for i in range(n - 1):
+    for j in range(n - i - 1):
+        if arr[j] > arr[j + 1]:
+            # Swap elements
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print("Sorted array:", arr)
