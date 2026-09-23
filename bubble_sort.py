1810a9a0ef3e3555d4cf8a41dfbb2b6b7b445a22
shreyassugur <shreyasramesh.cs24@bmsce.ac.in>
2026-09-23 13:21:43 +0530

    bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,8 +4,7 @@ n = len(arr)
 
 for i in range(n - 1):
     for j in range(n - i - 1):
-        if arr[j] > arr[j + 1]:
-            # Swap elements
+        if arr[j] > arr[j + 1]
             arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 print("Sorted array:", arr)
